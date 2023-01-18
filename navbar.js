99cd0328bae7787b5ba3098acafc567e0a88eacf
Ahantya <115478950+Ahantya@users.noreply.github.com>
2023-01-17 19:26:38 -0600

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -3,3 +3,23 @@ fetch('navbar.html')
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
   });
+
+window.addEventListener('load', function(){
+    var newsletterButton = document.getElementById("newsletter-button");
+    var newsletterModal = document.getElementById("newsletter-modal");
+    var closeModal = document.getElementsByClassName("close")[0];
+
+    newsletterButton.onclick = function() {
+      newsletterModal.style.display = "block";
+    }
+
+    closeModal.onclick = function() {
+      newsletterModal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == newsletterModal) {
+        newsletterModal.style.display = "none";
+      }
+    }
+});
